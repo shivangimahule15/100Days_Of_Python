05d501f551da42751629ba7946391cc312dc2fa2
shivangimahule15 <shivangi.mahule06@gmail.com>
2021-03-14 21:15:10 +0530

    updated with format function

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -25,7 +25,8 @@ number_of_people = int( input("How many people to split the bill? ") )
 
 total_bill += total_bill * ( tip_percentage / 100 )
 
-individual_bill_amount = round( total_bill / number_of_people , 2 )
+#individual_bill_amount = round( total_bill / number_of_people , 2 )
+individual_bill_amount ="{:.2f}".format(total_bill / number_of_people)
 
 print(f"Each person should pay: ${ individual_bill_amount }")
 
